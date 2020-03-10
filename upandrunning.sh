d2db7ff441a79340e8e9a5d54619d8f4ebf74c42
Jonas Pedersen <jonas@Jonass-MacBook-Pro.local>
2020-03-10 13:15:03 +0100

    New script. Not tested yet

---
+++ upandrunning.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# MacOS Catalina install
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s /Users/$USER/dotfiles/.tmux.conf /Users/$USER/.tmux.conf 
+
+brew install python3
+brew install neovim
+pip3 install --user --upgrade neovim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p /Users/$USER/.config/nvim
+ln -s /Users/$USER/dotfiles/.vimrc /Users/$USER/.config/nvim/init.vim
+# Inside neovim
+# :PlugInstall
+
+ln -s /Users/$USER/dotfiles/.zshrc /Users/$USER/.zshrc
+mkdir -p ~/.zsh
+cd ~/.zsh
+curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
